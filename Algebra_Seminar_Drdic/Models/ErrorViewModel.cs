97c4b6b4582478cca346aef7d7557f67985000bd
antdrd123 <antoniodrdic7@gmail.com>
2022-06-09 12:16:02 +0200

    Add project files.

---
+++ Algebra_Seminar_Drdic/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Algebra_Seminar_Drdic.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
