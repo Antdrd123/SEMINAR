14cf2f95a0633c7c12a9f2100d7c8d9efb7cebe5
antdrd123 <antoniodrdic7@gmail.com>
2022-07-28 15:16:33 +0200

    Simple API generated with 2 functions. Possible new upgrades.

---
+++ API/Controllers/ApiProductController.cs
@@ -0,0 +1,53 @@
+﻿using Algebra_Seminar_Drdic.Data;
+using Algebra_Seminar_Drdic.Interfaces;
+using Algebra_Seminar_Drdic.Models;
+using Algebra_Seminar_Drdic.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiProductController : ControllerBase
+    {
+        public readonly ProductRepo _repo;
+
+        public ApiProductController(ProductRepo repo)
+        {
+            _repo = repo;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> GetAllProducts()
+        {
+            try
+            {
+                return Ok(_repo.GetAllProducts());
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ActionResult<Product> GetProduct(int id)
+        {
+            try
+            {
+                var result = _repo.GetProductById(id);
+
+                if(result == null)
+                {
+                    throw new Exception("Rezultat nije pronađen!");
+                }
+                return Ok(result);
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Rezultat nije moguće prikazati!");
+            }
+        }
+     }
+}
---
+++ Algebra_Seminar_Drdic/Repositories/ProductRepo.cs
@@ -0,0 +1,26 @@
+﻿using Algebra_Seminar_Drdic.Data;
+using Algebra_Seminar_Drdic.Interfaces;
+using Algebra_Seminar_Drdic.Models;
+
+namespace Algebra_Seminar_Drdic.Repositories
+{
+    public class ProductRepo 
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductRepo(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _context.Products.ToList();
+        }
+
+        public Product GetProductById(int id)
+        {
+            var product = _context.Products.SingleOrDefault(p => p.Id == id);
+            return product;
+        }
+    }
+}
