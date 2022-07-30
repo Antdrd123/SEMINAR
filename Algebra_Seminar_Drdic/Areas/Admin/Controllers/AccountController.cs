703ccee9f2f1860751c9b75ce7cf8b1e281f2473
antdrd123 <antoniodrdic7@gmail.com>
2022-07-30 17:05:45 +0200

    Before User.Model test (with account controller).

---
+++ Algebra_Seminar_Drdic/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Algebra_Seminar_Drdic.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Algebra_Seminar_Drdic.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager,
+                                 ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+
+        }
+
+        
+        public IActionResult Index()
+        {
+            var users = _context.Users.ToList();
+
+            return View(users);
+        }
+    }
+}
